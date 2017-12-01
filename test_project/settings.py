9c2d88930b97f7cecb739e3fb654d461d5ef3a83
Oleksa Vasylenko <oleksa.vasylenko@gmail.com>
2017-12-01 19:25:02 +0200

    Update settings

--- test_project/settings.py
+++ test_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -83,17 +84,18 @@ WSGI_APPLICATION = 'test_project.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
-DATABASES = {
-   'default': {
-       'ATOMIC_REQUESTS': True,
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': 'test_project',
-       'USER': 'postgres',
-       'PASSWORD': 'postgres',
-       'HOST': 'localhost',
-       'PORT': '5432',
-   }
-}
+#DATABASES = {
+#   'default': {
+#       'ATOMIC_REQUESTS': True,
+#       'ENGINE': 'django.db.backends.postgresql',
+#       'NAME': 'test_project',
+#       'USER': 'postgres',
+#       'PASSWORD': 'postgres',
+#       'HOST': 'localhost',
+#       'PORT': '5432',
+#   }
+#}
+DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
